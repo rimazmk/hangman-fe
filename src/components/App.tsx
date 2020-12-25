4d03c2c0a546e129aeda29b6d2162987fb3d4689
Rimaz Khan <rimazk@umich.edu>
2020-12-25 02:03:48 -0500

    basic sockets

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../logo.svg";
 import "../css/App.css";
+import { io } from "socket.io-client";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [receviedMessage, setReceivedMessage] = useState("");
+  const socket = io("http://localhost:5000", { transports: ["websocket"] });
+  let counter = 0;
+
+  useEffect(() => {
+    socket.on("response", (msg: string) => {
+      console.log("message: " + msg);
+      console.log(counter++);
+      setReceivedMessage(msg);
+    });
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("message sent");
+    socket.emit("message", message);
+    setMessage("");
+    // socket.onAny((msg: string) => setReceivedMessage(msg));
+    // or with emit() and custom event names
+    // socket.emit('salutations', 'Hello!', { 'mr': 'john' }, Uint8Array.from([1, 2, 3, 4]));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{receviedMessage}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="message"
+          name="message"
+        ></input>
+      </form>
     </div>
   );
 }
