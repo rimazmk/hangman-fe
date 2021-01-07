3aaedc8d4a762e65d4c83de31caae87a18d98996
Srihari Srinivasan <ssrihari@umich.edu>
2021-01-07 03:33:37 -0500

    Prevent Standings From Updating On Player Leave

--- src/components/Standings.tsx
+++ src/components/Standings.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import { gameStateInterface } from "../hangman";
 
 const Standings = ({ gameState }: { gameState: gameStateInterface }) => {
+  const standings = useRef<string[]>([]);
   const places = [
     "FIRST",
     "SECOND",
@@ -12,25 +13,30 @@ const Standings = ({ gameState }: { gameState: gameStateInterface }) => {
     "SEVENTH",
     "EIGTH",
   ];
-  let wins = Object.entries(gameState.wins);
-  wins.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-  let prev = wins[0][1];
-  let uniq = 0;
-  let res: Array<string> = [];
-  for (let person of wins) {
-    if (person[1] !== prev) {
-      uniq++;
+
+  if (!standings.current.length) {
+    let wins = Object.entries(gameState.wins);
+    wins.sort(function (a, b) {
+      return b[1] - a[1];
+    });
+    let prev = wins[0][1];
+    let uniq = 0;
+    let res: Array<string> = [];
+    for (let person of wins) {
+      if (person[1] !== prev) {
+        uniq++;
+      }
+      prev = person[1];
+      let str = `${places[uniq]} PLACE: ${person[0]} with a score of ${person[1]}`;
+      res.push(str);
     }
-    prev = person[1];
-    let str = `${places[uniq]} PLACE: ${person[0]} with a score of ${person[1]}`;
-    res.push(str);
+
+    standings.current = res;
   }
 
   return (
     <div>
-      {res.map((place: string) => (
+      {standings.current.map((place: string) => (
         <p key={place}>{place}</p>
       ))}
     </div>
